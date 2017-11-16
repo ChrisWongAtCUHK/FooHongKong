18e048c36e54a35c2b48ab9bc8af1627ba80972d
chriswongatcuhk <wc38.wong@gmail.com>
2017-11-16 21:38:34 +0800

    1234567

--- bot.js
+++ bot.js
@@ -13,9 +13,15 @@ else {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-bot.onText(/^/, function (msg) {
+bot.onText(/\/start/, function (msg) {
   var name = msg.from.first_name;
   bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(function () {
+    // reply sent!
+  });
+});
+
+bot.onText(/\/help/, function (msg) {
+  bot.sendMessage(msg.chat.id, '1234567').then(function () {
     // reply sent!
   });
 });
